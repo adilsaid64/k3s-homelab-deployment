ccadd5a4f35cac146c833c5f26e4dc4fc4b64ebe
adil <adilsaid64@gmail.com>
2026-02-11 22:06:34 +0000

    feat: self hosting  excalidraw

--- app/infra/services/index.ts
+++ app/infra/services/index.ts
@@ -1,4 +1,4 @@
 export * from './proxmox';
-export * from './kubepromethues';
+export * from './k8s';
 export * from './proxmoxMaster';
 export * from './proxmoxWorkers';
---
+++ app/infra/services/k8s.ts
@@ -0,0 +1,133 @@
+
+import { KubePrometheusStack } from '../components/k8s/KubePrometheusStack';
+import { k3Master } from './proxmoxMaster';
+import { workerVms } from './proxmoxWorkers';
+import { Excalidraw } from "../components/k8s/Excalidraw";
+
+const k8sProvider = new kubernetes.Provider('k3s');
+
+export const kubePromStack = new KubePrometheusStack(
+    'monitoring-stack',
+    {
+        provider: k8sProvider,
+        namespace: 'monitoring',
+
+        grafana: {
+            enabled: true,
+            ingress: {
+                host: 'grafana.k3s.local',
+                className: 'traefik',
+            },
+            pod: {
+                resources: {
+                    requests: {
+                        cpu: '50m',
+                        memory: '128Mi',
+                    },
+                    limits: {
+                        cpu: '200m',
+                        memory: '256Mi',
+                    },
+                },
+            },
+        },
+
+        prometheus: {
+            retention: '5d',
+            scrapeInterval: '120s',
+            pod: {
+                resources: {
+                    requests: {
+                        cpu: '200m',
+                        memory: '512Mi',
+                    },
+                    limits: {
+                        cpu: '500m',
+                        memory: '1Gi',
+                    },
+                },
+            },
+        },
+
+        alertmanager: {
+            enabled: true,
+            pod: {
+                resources: {
+                    requests: {
+                        cpu: '25m',
+                        memory: '64Mi',
+                    },
+                    limits: {
+                        cpu: '100m',
+                        memory: '128Mi',
+                    },
+                },
+            },
+        },
+
+        extraValues: {
+            kubeStateMetrics: {
+                resources: {
+                    requests: {
+                        cpu: '50m',
+                        memory: '128Mi',
+                    },
+                    limits: {
+                        cpu: '150m',
+                        memory: '256Mi',
+                    },
+                },
+            },
+
+            prometheusOperator: {
+                resources: {
+                    requests: {
+                        cpu: '50m',
+                        memory: '128Mi',
+                    },
+                    limits: {
+                        cpu: '200m',
+                        memory: '256Mi',
+                    },
+                },
+            },
+
+            nodeExporter: {
+                resources: {
+                    requests: {
+                        cpu: '10m',
+                        memory: '32Mi',
+                    },
+                    limits: {
+                        cpu: '50m',
+                        memory: '64Mi',
+                    },
+                },
+            },
+        },
+    },
+    { dependsOn: [k3Master, workerVms[0].vm] },
+);
+
+export const excalidraw = new Excalidraw("excalidraw", {
+    provider: k8sProvider,
+    namespace: "exclaidraw",
+    ingress: {
+        enabled: true,
+        host: "draw.k3s.local",
+        className: "traefik",
+    },
+    pod: {
+        resources: {
+            requests: {
+                cpu: "50m",
+                memory: "64Mi",
+            },
+            limits: {
+                cpu: "200m",
+                memory: "128Mi",
+            },
+        },
+    },
+});
+
